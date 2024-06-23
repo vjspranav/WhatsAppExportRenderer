533ea476f43c396decd0b2fe2b69ae9c9d7cea9c
vjspranav <pranavasri@live.in>
2024-06-24 01:08:20 +0530

    Add date logic * Missed adding it

--- js/main.js
+++ js/main.js
@@ -107,3 +107,32 @@ messages.forEach(message => {
 
     chatContainer.appendChild(parentDiv);
 });
+
+const dateHeader = document.getElementById('dateHeader');
+let currentVisibleDate = null;
+
+// Function to update the date header based on the current visible date
+function updateDateHeader() {
+    dateHeader.textContent = currentVisibleDate || '';
+}
+
+chatContainer.addEventListener('scroll', () => {
+    const messageDivs = document.querySelectorAll('.chat-message');
+
+    // Find the first visible message on top
+    for (let i = messageDivs.length - 1; i >= 0; i--) {
+        const rect = messageDivs[i].getBoundingClientRect();
+        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+            // date is of format dd/mm/yyyy
+            // convert it to mm/dd/yyyy
+            let timestamp = messages[i].timestamp.split('/');
+            timestamp = `${timestamp[1]}/${timestamp[0]}/${timestamp[2]}`;
+            currentVisibleDate = new Date(timestamp).toLocaleDateString();
+            // convert back
+            currentVisibleDate = currentVisibleDate.split('/');
+            currentVisibleDate = `${currentVisibleDate[1]}/${currentVisibleDate[0]}/${currentVisibleDate[2]}`;
+            updateDateHeader();
+            break;
+        }
+    }
+});
